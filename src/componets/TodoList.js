26bf063a1f13646fc2ba55882493314d4d41a32c
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2021-01-17 19:01:23 +0200

    add logic check/del todo, edit css

--- src/componets/TodoList.js
+++ src/componets/TodoList.js
@@ -1,10 +1,17 @@
 import { TodoItem } from './TodoItem';
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos, deleteTodo, completedTodo }) => {
   const isTodosExist = todos && todos.length > 0;
   const listItems = isTodosExist
     ? todos.map(({ id, text, completed }) => (
-        <TodoItem key={`todo-${id}`} text={text} completed={completed} />
+        <TodoItem
+          key={`todo-${id}`}
+          text={text}
+          completed={completed}
+          deleteTodo={deleteTodo}
+          completedTodo={completedTodo}
+          id={id}
+        />
       ))
     : 'Todo list is empty';
 
--- src/store/todos/todosSlice.js
+++ src/store/todos/todosSlice.js
@@ -1,9 +1,13 @@
 const TODOS_TODO_ADD = 'todos/todoAdd';
+const TODOS_TODO_COMPLETED = 'todos/todoCompleted';
+const TODOS_TODO_DEL = 'todos/todoDel';
+
+let idTodoCounter = 1;
 
 const todosState = [
   {
     id: 0,
-    text: 'todo as todoState (init)',
+    text: 'todo init state',
     completed: false,
   },
 ];
@@ -13,16 +17,40 @@ export const addTodo = text => ({
   payload: text,
 });
 
+export const completedTodo = id => ({
+  type: TODOS_TODO_COMPLETED,
+  payload: id,
+});
+
+export const deleteTodo = id => ({
+  type: TODOS_TODO_DEL,
+  payload: id,
+});
+
 export const todosReducer = (state = todosState, action) => {
   if (action.type === TODOS_TODO_ADD) {
     return [
       ...state,
       {
-        id: state.length, //temp id
+        id: idTodoCounter++, //temp id
         text: action.payload,
         completed: false,
       },
     ];
+  }
+
+  if (action.type === TODOS_TODO_COMPLETED) {
+    return [...state].map(todo => {
+      if (todo.id === action.payload) {
+        todo.completed = !todo.completed;
+      }
+
+      return todo;
+    });
+  }
+
+  if (action.type === TODOS_TODO_DEL) {
+    return [...state].filter(todo => todo.id !== action.payload);
   }
 
   return state;
